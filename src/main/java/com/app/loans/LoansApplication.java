c5d450659b66dcd65de3609084d090029593eefb
Jaya Krishna Mannem <jayamannem07@gmail.com>
2025-02-12 15:39:50 +0530

    The application now sources it's properties files from an external configserver (not completely)

--- src/main/java/com/app/loans/LoansApplication.java
+++ src/main/java/com/app/loans/LoansApplication.java
@@ -2,11 +2,15 @@ package com.app.loans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.app.loans.dto.LoansContactInfoDto;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = { LoansContactInfoDto.class })
 public class LoansApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LoansApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(LoansApplication.class, args);
+  }
 }
